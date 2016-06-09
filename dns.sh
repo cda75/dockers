fef6597941796bbc6c8ecc115b91565f52af8234
cda75 <d.cestnov@gmail.com>
2016-06-09 10:29:23 +0300

    edit1

--- dns.sh
+++ dns.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 dns_pid=`docker ps -a | grep "dns$" | awk '{print $1;}'`
 if [ -z $dns_pid ];
 then
- docker rm -f $pid && docker run -d --name=dns --dns=127.0.0.1 --publish=53:53/udp --publish=10000:10000 --volume=/srv/docker/bind:/data --env='ROOT_PASSWORD=P@ssw0rd' cda75/dns:latest
+ docker run -d --name=dns --dns=127.0.0.1 --publish=53:53/udp --publish=10000:10000 --volume=/srv/docker/bind:/data --env='ROOT_PASSWORD=P@ssw0rd' cda75/dns:latest
 else
  docker kill $dns_pid
  docker rm -f $dns_pid
