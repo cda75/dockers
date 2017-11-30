0ff3446ea19ab4f4b14f4bd61793c3718e265d45
dimka <dimka@docker>
2017-11-30 12:24:27 +0300

    addes install docker for debian

--- install_docker.sh
+++ install_docker.sh
@@ -1,12 +1,11 @@
 sudo apt-get remove docker docker-engine
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-os_name=`lsb_release –I | awk ‘{print $3}’`
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$os_name $(lsb_release -cs) stable"
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install -y docker-ce 
 #sudo groupadd docker
 #sudo usermod -aG docker $USER
 #logout
-sudo systemctl enable docker
+#sudo systemctl enable docker
 
